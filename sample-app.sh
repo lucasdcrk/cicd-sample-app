33463c44e0fca626014b492b77cd1ee61e1e4e6d
Lucas Decrock <lucas@decrock.me>
2022-11-07 10:56:40 +0100

    Pipeline fix

--- sample-app.sh
+++ sample-app.sh
@@ -22,5 +22,7 @@ _EOF_
 
 cd tempdir || exit
 docker build -t sampleapp .
+docker stop samplerunning
+docker rm samplerunning
 docker run -t -d -p 5050:5050 --name samplerunning sampleapp
 docker ps -a 
